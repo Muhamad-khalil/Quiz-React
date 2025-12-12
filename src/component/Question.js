0f54efdf6dc5b79b763d770b03269ac334d7bc5e
Muhamad-khalil <medo.khalil.01.18@gmail.com>
2025-12-13 01:41:17 +0200

    Displaying Questions

---
+++ src/component/Question.js
@@ -0,0 +1,14 @@
+import Options from "./Option";
+
+function Question({ question }) {
+  console.log(question);
+
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options question={question} />
+    </div>
+  );
+}
+
+export default Question;
